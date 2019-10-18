9847dbdea3393fda0c5b8e6fe18febff614240e1
Scott Slatton <scottslatton.tech@gmail.com>
2019-10-17 17:21:18 -0700

    played with converting double to int.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Casting_and_Converting
     {
         static void Main(string[] args)
         {
-            
+            int num = 789702395;
+            long bigNum = num;
+
+            // Works fine, no danger of data loss
+
+            int i = 5;
+            double d = 10.5;
+
+            i = (int)d;  // Must explicitly cast
+
+            Console.WriteLine("The Double variable d converted to int: " + i);
         }
     }
 }
